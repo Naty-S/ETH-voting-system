c61f8446eafe954c4df5526d91ae1ace5717f552
Naty-S <na7a5cha010@gmail.com>
2022-01-11 12:16:26 -0400

    vars

---
+++ config/variables.py
@@ -0,0 +1,15 @@
+# Cantidad de localidades
+LOCALS = 5
+
+# Cantidad de centros de votación por localidad
+CENTERS = 3
+
+# Cantidad de votantes por localidad
+VOTERS_1 = 5
+VOTERS_2 = 6
+VOTERS_3 = 3
+VOTERS_4 = 8
+VOTERS_5 = 5
+
+# Nivel de concurrencia de envío de votos
+CONCURRENCY = 3
