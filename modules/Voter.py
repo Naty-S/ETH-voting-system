3978417f65d04b5e483655b5923155c3210f1240
Naty-S <na7a5cha010@gmail.com>
2022-01-11 21:17:00 -0400

    cargo

--- modules/Voter.py
+++ modules/Voter.py
@@ -1,20 +1,16 @@
 import eth_keys      as eth
 
-import modules.Vote as vote
 
 class Voter:
 
-  def __init__(self, voter:int, locality: str, center: int) -> None:
+  def __init__(self, voter: int, locality: str, center: int, cargo: int = -1) -> None:
     self.center   = "Centro" + center
     self.locality = locality
     self.name     = "Votante" + voter
+    # Asamblea 0, Congreso 1
+    self.cargo    = cargo
     self.wei      = 10000000
     self.mail     = self.name + "@gmail.com"
     self.privKey  = eth.keys.PrivateKey(b'\x01' * 32)
     self.publKey  = self.privKey.public_key
     self.address  = self.publKey.to_checksum_address()
-    self.vote     = None
-  
-
-  def vote(self):
-    self.vote = vote.Vote()
